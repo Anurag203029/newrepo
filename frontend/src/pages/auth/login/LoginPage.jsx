fc75c26c34b07daf49491563eb4206c76b4bdffd
anurag <anuragvig2@gmail.com>
2024-09-13 16:06:29 +0530

    Your commit message here

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -29,7 +29,7 @@ const LoginPage = () => {
 	} = useMutation({
 		mutationFn: async ({ username, password }) => {
 			try {
-				const res = await fetch("/api/auth/login", {
+				const res = await fetch("https://wegram-working-repo.onrender.com/api/auth/login", {
 					method: "POST",
 					headers: {
 						"Content-Type": "application/json",
--- frontend/src/pages/auth/signup/SignUpPage.jsx
+++ frontend/src/pages/auth/signup/SignUpPage.jsx
@@ -20,7 +20,7 @@ const SignUpPage = () => {
 	const { mutate, isError, isPending, error } = useMutation({
 		mutationFn: async ({ email, username, fullName, password }) => {
 			try {
-				const res = await fetch("/api/auth/signup", {
+				const res = await fetch("https://wegram-working-repo.onrender.com/api/auth/signup", {
 					method: "POST",
 					headers: {
 						"Content-Type": "application/json",
